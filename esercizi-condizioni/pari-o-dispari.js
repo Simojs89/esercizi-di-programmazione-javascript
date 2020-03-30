c74fd1072251af5fb2babc02373420c456eb41cf
Simojs89 <simone.d89l@gmail.com>
2020-03-30 12:35:59 +0200

    Update pari-o-dispari.js

--- esercizi-condizioni/pari-o-dispari.js
+++ esercizi-condizioni/pari-o-dispari.js
@@ -10,8 +10,15 @@
     Output: 0
 
   Consigli:
-  Per capire se un numero è pari o dispari puoi usare un operatore, ma quale? :) 
+  Per capire se un numero è pari o dispari puoi usare un operatore, ma quale? :)
   (Se non ti ricordi riguarda la lezione "Come dichiarare e definire le variabili e i tipi di dato" oppure "Il tipo Numero e gli operatori aritmetici")
 
   http://www.imparareaprogrammare.it
 */
+var a = 63;
+if (a%2) {
+  console.log(`1`)
+
+}else {console.log(`0`)
+
+}
