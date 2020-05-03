4ad712c2fd1009493a1a995a8e1488508fab851c
Simojs89 <simone.d89l@gmail.com>
2020-05-03 17:57:24 +0200

    Update maggiore-e-minore.js

--- esercizi-condizioni/maggiore-e-minore.js
+++ esercizi-condizioni/maggiore-e-minore.js
@@ -6,7 +6,17 @@
   Esempio:
     Input: a = 3, b = -1, c = 4, d = -2
     Output: maggiore = 4, minore = -2
-    
+
 
   http://www.imparareaprogrammare.it
 */
+var a=prompt("inserisci un valore");
+var b=prompt("inserisci un valore");
+var c=prompt("inserisci un valore");
+var d=prompt("inserisci un valore");
+
+var max= Math.max(a,b,c,d);//Con questo dato otteniamo il numero maggiore che inseriremo nel browser
+var min= Math.min(a,b,c,d);//Con questo dato otteniamo il numero minore che inseriremo nel browser
+
+console.log ('maggiore=',max);
+console.log ("minore=",min );
