efae6c47ecf18e472406efa39c8874fcf4418fbb
Simojs89 <simone.d89l@gmail.com>
2020-05-25 23:13:52 +0200

    Update primo-e-ultimo.js

--- esercizi-funzioni/primo-e-ultimo.js
+++ esercizi-funzioni/primo-e-ultimo.js
@@ -13,3 +13,15 @@
 
   http://www.imparareaprogrammare.it
 */
+function minimo(array) {//come parametro della funzione, inseriremo un array
+  //il return che avremo sarà il valore minimo o nel caso di sotto quello massimo, utilizzando il metodo apply nel caso di un numero.
+  return Math.min.apply(null, array);
+}
+ 
+function massimo(array) {
+  return Math.max.apply(null, array);
+}
+ 
+console.log(`il numero minimo è :`+(minimo([prompt(),prompt(),prompt()]))); 
+
+console.log(`il numero massimo è: `+(massimo([prompt(),prompt(),prompt()])));
