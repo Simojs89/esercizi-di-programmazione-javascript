0559ebc7e6c9b6c347954e8212cba19e76ba6cfd
Simojs89 <simone.d89l@gmail.com>
2020-07-01 22:03:08 +0200

    Update verifica-la-data.js

--- esercizi-oggetti/verifica-la-data.js
+++ esercizi-oggetti/verifica-la-data.js
@@ -13,3 +13,35 @@
 
   http://www.imparareaprogrammare.it
 */
+function date(day,month,year){
+  this.day = "",
+  this.month="",
+  this.year="",
+  this.description= ()=> {
+      if (this.day>31) {
+          console.log("data non valida")
+          
+          
+      }else if (this.month>12) {
+          console.log("data non valida")
+
+          
+      }else if (this.year>2050) {
+          console.log("data non valida")
+
+          
+      }
+      else{
+      console.log(`${this.day} ${this.month} ${this.year}`)}
+  
+  
+      }}
+
+
+let data=new date
+data.day=prompt("insert a day")
+data.month=prompt("insert a month")
+data.year=prompt("insert a year")
+
+
+data.description();
